914673a317928d84d59ed022e777d975d7bc8784
anandku06 <161126335+anandku06@users.noreply.github.com>
2025-07-31 01:47:28 +0530

    Implement updateBook controller with error handling

--- BookStore_API/controllers/book-controllers.js
+++ BookStore_API/controllers/book-controllers.js
@@ -68,7 +68,37 @@ const addNewBook = async (req, res) => {
   }
 };
 
-const updateBook = async (req, res) => {};
+const updateBook = async (req, res) => {
+  try {
+    const updatedData = req.body;
+    const updatedBook = await Book.findByIdAndUpdate(
+      req.params.id,
+      updatedData,
+      {
+        new: true,
+      }
+    );
+
+    if (!updatedBook) {
+      res.status(404).json({
+        success: false,
+        message: "Book with this ID not found!!",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Book updated successfully",
+      data: updatedBook,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Something went wrong!!",
+    });
+  }
+};
 
 const deleteBook = async (req, res) => {
   try {
