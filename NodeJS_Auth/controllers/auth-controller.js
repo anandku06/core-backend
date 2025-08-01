7a733b932de7c454f1bc5523e6ad190c116df424
anandku06 <161126335+anandku06@users.noreply.github.com>
2025-08-02 02:11:00 +0530

    Implement user registration and login controllers with error handling

---
+++ NodeJS_Auth/controllers/auth-controller.js
@@ -0,0 +1,23 @@
+// register User
+const registerUser = async (res, req) => {
+  try {
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: true,
+      message: "Something went wrong!!",
+    });
+  }
+};
+
+// login user 
+const loginUser = async (res, req) => {
+  try {
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: true,
+      message: "Something went wrong!!",
+    });
+  }
+};
