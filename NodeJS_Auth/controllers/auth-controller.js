854ef5a538fdbf5adf8871c65bd4cca078e6a6fb
anandku06 <161126335+anandku06@users.noreply.github.com>
2025-08-13 14:46:22 +0530

    Implement user registration error handling and add bcryptjs dependency

--- NodeJS_Auth/controllers/auth-controller.js
+++ NodeJS_Auth/controllers/auth-controller.js
@@ -1,6 +1,20 @@
+const User = require("../models/User");
+
 // register User
 const registerUser = async (res, req) => {
   try {
+    // extract user info
+    const { username, email, password, role } = req.body;
+
+    // check existing User in Db
+    const userExists = await User.findOne({ $or: [{ username }, { email }] }); // or checks for two values (username or email)
+
+    if (userExists) {
+      return res.status(400).json({
+        success: false,
+        message: "User already exists, try with different username or email",
+      });
+    }
   } catch (error) {
     console.log(error);
     res.status(500).json({
