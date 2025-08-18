5f58a05a27028972ef73f86f8127ba926c4bc141
anandku06 <161126335+anandku06@users.noreply.github.com>
2025-08-18 12:39:09 +0530

    Implement authentication middleware for protected routes and handle token verification

---
+++ NodeJS_Auth/middlewares/auth-middleware.js
@@ -0,0 +1,31 @@
+const jwt = require('jsonwebtoken')
+
+
+const authMiddleware = (req, res, next) => {
+
+    const authHeaders = req.headers["authorization"]
+    const token = authHeaders && authHeaders.split(" ")[1] // check whether authHeaders exist or not, if yes then only split
+    if (!token){
+        return res.status(401).json({
+            success : false,
+            message : 'Access denied. Please login to continue!'
+        })
+    }
+
+    // decode this token
+    try {
+        const decodedToken = jwt.verify(token, process.env.JWT_SECRET_KEY) // Synchronously verify given token using a secret or a public key to get a decoded token token
+
+        req.userInfo = decodedToken
+        next()
+
+    } catch (error) {
+        return res.status(500).json({
+            success : false,
+            message : "Something went wrong!"
+        })
+    }
+
+}
+
+module.exports = authMiddleware
--- NodeJS_Auth/routes/home-routes.js
+++ NodeJS_Auth/routes/home-routes.js
@@ -1,11 +1,15 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const authMiddleware = require("../middlewares/auth-middleware");
+const router = express.Router();
 
 // to protect this route, we attach a handler function, that'll check whether the user is authenticated or not before visiting this route
-router.get('/', (req, res) => {
-    res.json({
-        message : "Welcome to the Home Page!"
-    })
-})
+// the get method can take handler functions that are used for auth, and much more things
 
-module.exports = router
+router.get("/", authMiddleware, (req, res) => {
+  res.json({
+    message: "Welcome to the Home Page!",
+  });
+});
+// now whenever this route is called, the handler function "authMiddleware" will be called before the actual response
+
+module.exports = router;
