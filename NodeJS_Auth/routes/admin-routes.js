d8ccb1d40ea7275a56292e2c255de01ac80ce7e8
anandku06 <161126335+anandku06@users.noreply.github.com>
2025-08-27 00:23:25 +0530

    Add admin routes and integrate with server for admin access

---
+++ NodeJS_Auth/routes/admin-routes.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const authMiddleware = require("../middlewares/auth-middleware")
+const router = express.Router()
+
+
+router.get('/welcome', authMiddleware, (req, res) => {
+    res.json({
+        message : 'Welcome to the admin page!'
+    })
+})
+
+module.exports = router
--- NodeJS_Auth/server.js
+++ NodeJS_Auth/server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const connectToDB = require("./database/db");
 const authRoutes = require("./routes/auth-routes");
 const homeRoutes = require("./routes/home-routes");
+const adminRoutes = require("./routes/admin-routes")
 
 connectToDB();
 const app = express();
@@ -11,6 +12,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json())
 
 app.use("/api/home", homeRoutes);
+app.use("/api/admin", adminRoutes);
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
