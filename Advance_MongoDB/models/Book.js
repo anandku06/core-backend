942db1c3e8936431bcd92fba6d0e4ac80097c67a
anandku06 <161126335+anandku06@users.noreply.github.com>
2025-09-21 10:03:47 +0530

    Add Author and Book models with schemas for MongoDB

---
+++ Advance_MongoDB/models/Book.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose")
+
+
+const bookSchema = new mongoose.Schema({
+    title: String,
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Author"
+    }
+})
+
+module.exports = mongoose.model("Book", bookSchema)
