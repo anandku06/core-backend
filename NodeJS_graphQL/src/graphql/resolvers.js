4593d82eee3bfc2350cc2c80b19334a41906c3cd
anandku06 <161126335+anandku06@users.noreply.github.com>
2025-10-09 23:43:58 +0530

    fix: correct product data import and update module exports

--- NodeJS_graphQL/src/graphql/resolvers.js
+++ NodeJS_graphQL/src/graphql/resolvers.js
@@ -1,12 +1,12 @@
-const products = require("../data/products");
+const { products } = require("../data/products.js");
 
 // contains the resolver functions for the GraphQL API
 
 const resolvers = {
   Query: {
     products: () => products,
+    product : (_, {id}) => products.find(product => product.id === id)
   },
 };
 
-
-module.exports = { resolvers };
+module.exports = resolvers;
