07132b04a72d8cc62a828ca040d7809a317b5121
Anand Kumar <rishavgupta163@gmail.com>
2025-10-17 11:52:04 +0530

    feat: set up Apollo Server with initial schema and resolvers files

---
+++ graphQL_Practice/src/server.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const { ApolloServer } = require("@apollo/server"); // to create the Apollo Server
+const { startStandaloneServer } = require("@apollo/server/standalone"); // to run the server standalone
+const typeDefs = require("./graphql/schema.js");
+const resolvers = require("./graphql/resolvers.js");
+
+async function startServer() {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  }); // create an instance of ApolloServer with the schema and resolvers
+
+  const { url } = await startStandaloneServer(server, {
+    listen: { port: process.env.PORT },
+  });
+
+  console.log(`Server running at ${url}`);
+}
+
+startServer(); // entry point of the application
+// sets up and starts the Apollo Server
