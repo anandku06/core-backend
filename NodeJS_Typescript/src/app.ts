b1e8060b9d13fc02adf76b9bed6f1ff9c063c22e
Anand Kumar <rishavgupta163@gmail.com>
2025-10-21 19:42:46 +0530

    fix: update import alias for express and adjust app initialization

--- NodeJS_Typescript/src/app.ts
+++ NodeJS_Typescript/src/app.ts
@@ -1,8 +1,11 @@
-import express from 'express';
+import e from 'express';
 
-const app: express.Express = express();
+const app: e.Express = e();
 const PORT = 3000;
 
+app.get('/', (req: e.Request, res: e.Response) => {
+    res.send('Hello, TypeScript with Express!');
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
